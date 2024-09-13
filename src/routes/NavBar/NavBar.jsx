4324796631efb7a5ccaa3b0f4a3dba22f1fd8a08
hpham474 <hpham474@gmail.com>
2024-09-13 02:01:02 -0500

    add nav bar styling

--- src/routes/NavBar/NavBar.jsx
+++ src/routes/NavBar/NavBar.jsx
@@ -5,15 +5,81 @@ function NavBar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.topNav}>
-        <h1>Logo</h1>
-        <NavLink to="cart">Shopping Cart</NavLink>
+        <h1 className={styles.logo}>Shopping Site</h1>
+        <NavLink
+          to="cart"
+          className={({ isActive, isPending }) =>
+            isActive
+              ? styles.cartLinkActive
+              : isPending
+              ? styles.cartLinkPending
+              : styles.cartLink
+          }
+        >
+          Shopping Cart
+        </NavLink>
       </div>
       <div className={styles.bottomNav}>
-        <NavLink to="">Home</NavLink>
-        <NavLink to="mens">Men&apos;s Clothing</NavLink>
-        <NavLink to="womens">Women&apos;s Clothing</NavLink>
-        <NavLink to="jewelery">Jewelery</NavLink>
-        <NavLink to="electronics">Electronics</NavLink>
+        <NavLink
+          to=""
+          className={({ isActive, isPending }) =>
+            isActive
+              ? styles.categoryLinksActive
+              : isPending
+              ? styles.categoryLinksPending
+              : styles.categoryLinks
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="mens"
+          className={({ isActive, isPending }) =>
+            isActive
+              ? styles.categoryLinksActive
+              : isPending
+              ? styles.categoryLinksPending
+              : styles.categoryLinks
+          }
+        >
+          Men&apos;s Clothing
+        </NavLink>
+        <NavLink
+          to="womens"
+          className={({ isActive, isPending }) =>
+            isActive
+              ? styles.categoryLinksActive
+              : isPending
+              ? styles.categoryLinksPending
+              : styles.categoryLinks
+          }
+        >
+          Women&apos;s Clothing
+        </NavLink>
+        <NavLink
+          to="jewelery"
+          className={({ isActive, isPending }) =>
+            isActive
+              ? styles.categoryLinksActive
+              : isPending
+              ? styles.categoryLinksPending
+              : styles.categoryLinks
+          }
+        >
+          Jewelery
+        </NavLink>
+        <NavLink
+          to="electronics"
+          className={({ isActive, isPending }) =>
+            isActive
+              ? styles.categoryLinksActive
+              : isPending
+              ? styles.categoryLinksPending
+              : styles.categoryLinks
+          }
+        >
+          Electronics
+        </NavLink>
       </div>
     </nav>
   );
