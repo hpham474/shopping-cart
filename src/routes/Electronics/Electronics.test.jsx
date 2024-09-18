b3ea7998111ab75ffa15fdb38aeca450423ecb1d
hpham474 <hpham474@gmail.com>
2024-09-18 04:47:38 -0500

    add test files for each route

---
+++ src/routes/Electronics/Electronics.test.jsx
@@ -0,0 +1,38 @@
+import { vi, describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Electronics from "./Electronics";
+
+describe("Electronics", () => {
+  it("display loading state initially", () => {
+    window.fetch = vi.fn(() => {
+      const items = [
+        { id: 1, title: "test item 1" },
+        { id: 2, title: "test item 2" },
+      ];
+
+      return Promise.resolve({ json: () => Promise.resolve(items) });
+    });
+
+    render(<Electronics />);
+    const loadingElement = screen.getByText(/Loading items.../i);
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  it("display fetched data after loading", async () => {
+    window.fetch = vi.fn(() => {
+      const items = [
+        { id: 1, title: "test item 1" },
+        { id: 2, title: "test item 2" },
+      ];
+
+      return Promise.resolve({ json: () => Promise.resolve(items) });
+    });
+
+    render(<Electronics />);
+    const item1 = await screen.findAllByText(/test item 1/i);
+    const item2 = await screen.findAllByText(/test item 2/i);
+
+    expect(item1.length).toBeGreaterThan(0);
+    expect(item2.length).toBeGreaterThan(0);
+  });
+});
