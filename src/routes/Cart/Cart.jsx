d879ab13e47278fdb07291c7fc49897f1cb6defe
hpham474 <hpham474@gmail.com>
2024-09-23 21:01:51 -0500

    display total cost in cart, center layout

--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -12,23 +12,42 @@ function Cart() {
     return false;
   }
 
+  function totalPrice() {
+    let totalCost = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalCost = totalCost + cart[i].item.price * cart[i].quantity;
+    }
+
+    return totalCost;
+  }
+
   return isEmpty() ? (
     <p className={styles.empty}>No Items In Shopping Cart</p>
   ) : (
     <div className={styles.content}>
       <h2 className={styles.title}>Shopping Cart</h2>
-      <div className={styles.list}>
-        {cart.map((item) => {
-          return (
-            <CartTile
-              key={item.item.id}
-              item={item.item}
-              cart={cart}
-              cartChange={cartChange}
-              cartRemove={cartRemove}
-            ></CartTile>
-          );
-        })}
+      <div className={styles.shoppingCart}>
+        <div className={styles.list}>
+          {cart.map((item) => {
+            return (
+              <CartTile
+                key={item.item.id}
+                item={item.item}
+                cart={cart}
+                cartChange={cartChange}
+                cartRemove={cartRemove}
+              ></CartTile>
+            );
+          })}
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.checkOut}>
+            <div className={styles.price}>
+              Total: ${parseFloat(totalPrice()).toFixed(2)}
+            </div>
+            <button className={styles.checkOutButton}>Check Out</button>
+          </div>
+        </div>
       </div>
     </div>
   );
